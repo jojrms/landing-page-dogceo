4702d6f052627bebf20f215a1f205a8f6524e801
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-08-03 17:43:47 -0400

    💣 CODIFICAÇÃO - Finalizando header e iniciando section inicial

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,36 @@
-export default async function Home(){
+/* eslint-disable jsx-a11y/alt-text */
+const Home = () => {
 
     return(
-        <></>
+        <>
+            <header>
+                <span>
+                    <p>WANT EXTRA DISCOUNT LEARN MORE</p>
+                    <p>FREE SHIPPING ON ALL ORDERS $21+</p>
+                    <input type='button' value={'HELP'}/>
+                    <img width={'25vw'} src="https://img.icons8.com/ios-glyphs/90/FFFFFF/search--v1.png"/>
+                    <img width={'30vw'} src="https://img.icons8.com/windows/90/FFFFFF/shopping-cart.png"/>
+                </span>
+                <section>
+                    <i/>
+                    <ul>
+                        <li>Shop All</li>
+                        <li>How i works</li>
+                        <li>Reviews</li>
+                        <li>Contact</li>
+                    </ul>
+                    <input type='button' value={'Log In'}/>
+                </section>
+
+            </header>
+            <section className="sectionInitial">
+                <aside></aside>
+                <aside>
+                    <i/>
+                </aside>
+            </section>
+        </>
     )
 }
+
+export default Home;
