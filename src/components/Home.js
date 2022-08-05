8ec47bdd2adb26993fc4b3154af0e40acd6d766f
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-08-05 09:01:04 -0400

    💣 CODIFICAÇÃO - Ajuste na imagem exibida no primeiro section

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,7 +24,30 @@ const Home = () => {
 
             </header>
             <section className="sectionInitial">
-                <aside></aside>
+                <aside>
+                    <p>#AdoptionSave</p>
+                    <h1><b>Animals save us. <br/>Now save them!</b></h1>
+                    <h2>Dogs are one of the animals that most live in street situations 
+                        after family abandonment. <br/> Unbridled procreation increases this 
+                        population and our mission is to give a definitive <br/> and loving home 
+                        to all rescued.
+                    </h2>
+                    <div>
+                        <span>
+                            <img src="https://img.icons8.com/material-rounded/90/000000/plus--v1.png"/>
+                            <select>
+                                <option>$10.00</option>
+                                <option>$15.00</option>
+                                <option>$20.00</option>
+                                <option>Other</option>
+                            </select>
+                        </span>
+                        <span>
+                            <img src="https://img.icons8.com/fluency-systems-filled/96/FFFFFF/wallet.png"/>
+                            <input type={'button'} value={'Donate Now'}/>
+                        </span>
+                    </div>
+                </aside>
                 <aside>
                     <i/>
                 </aside>
